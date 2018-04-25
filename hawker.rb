f504e0122562dd2babea31b72fa7512e36156496
Karthikeyan A K <77minds@gmail.com>
2018-04-25 06:20:24 +0530

    added hawker

---
+++ hawker.rb
@@ -0,0 +1,9 @@
+mangoes = 200
+buy_price = 20
+sell_price = 55
+
+profit_per_mango = sell_price - buy_price
+total_profit = mangoes * profit_per_mango
+
+puts "By selling #{mangoes} magoes today, the hawker
+has made a profit of Rs #{total_profit}/-"
